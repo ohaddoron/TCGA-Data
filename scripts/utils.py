1370f9dc0eaa653a322f8935d303133e194bb0e9
Ohad Doron <ogdoron@gmail.com>
2022-05-03 22:08:28 +0300

    added mongodb insertion scripts working for mRNA

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,8 +1,16 @@
+import csv
 import os.path
 import subprocess
+from abc import ABC, abstractmethod
+from io import StringIO
 from pathlib import Path
 
+import pymongo
+import requests
+import toml
 import typer
+from pymongo import IndexModel
+from tqdm import tqdm
 from typer import Typer
 import pandas as pd
 import numpy as np
@@ -53,6 +61,138 @@ def run_gdc_client_download_on_directory(
         for p in procs:
             p.wait()
 
+
+class AbstractDatabaseInserter(ABC):
+    def __init__(self, subject: str, base_dir: str, mongo_connection_string: str, db_name: str, col_name: str):
+        self.subject = subject
+        self.base_dir = base_dir
+        self.mongo_connection_string = mongo_connection_string
+        self.db_name = db_name
+
+        self.info = self.request_file_info()
+        logger.debug(self.info.head())
+        self.patient_file_map = self.make_patient_file_map(self.info, base_dir)
+        client = pymongo.MongoClient(mongo_connection_string)
+        logger.debug(client.server_info())
+        db = client[db_name]
+        self.col = db[col_name]
+
+        self.col.drop()
+
+        indexes = [IndexModel([('name', pymongo.ASCENDING)]),
+                   IndexModel([('patient', pymongo.ASCENDING)]),
+                   IndexModel([('sample', pymongo.ASCENDING)]),
+                   IndexModel([('patient', pymongo.ASCENDING), ('name', pymongo.ASCENDING)]),
+                   IndexModel([('sample', pymongo.ASCENDING), ('name', pymongo.ASCENDING)]),
+                   IndexModel([('sample', pymongo.ASCENDING), ('patient', pymongo.ASCENDING)])
+                   ]
+
+        self.col.create_indexes(indexes)
+
+        for patient, file_path in tqdm(self.patient_file_map.items()):
+            self.insert_patient_data(patient=patient, file_path=file_path)
+
+    @abstractmethod
+    def insert_patient_data(self, patient: str, file_path: str):
+        ...
+
+    @abstractmethod
+    def request_file_info(self) -> pd.DataFrame:
+        ...
+
+    @staticmethod
+    def make_patient_file_map(df, base_dir):
+        return {row['cases.0.submitter_id']: os.path.join(
+            base_dir, row.id, row.file_name)
+            for _, row in df.iterrows()}
+
+
+class mRNADatabaseInserter(AbstractDatabaseInserter):
+    def insert_patient_data(self, patient: str, file_path: str):
+        with open(file_path) as f:
+            reader = csv.reader(f, delimiter='\t')
+            data = list(reader)
+            columns = data[1]
+            samples = []
+            for sample, row in enumerate(data[6:]):
+                for i in range(len(row)):
+                    samples.append(
+                        {'name': columns[i], 'value': row[i], 'patient': patient, 'sample': f'{patient}-{sample:05}'})
+
+        self.col.insert_many(samples)
+
+    def request_file_info(self) -> pd.DataFrame:
+        fields = [
+            "file_name",
+            "cases.submitter_id",
+            "cases.samples.sample_type",
+            "cases.project.project_id",
+            "cases.project.primary_site",
+        ]
+
+        fields = ",".join(fields)
+
+        files_endpt = "https://api.gdc.cancer.gov/files"
+
+        filters = {
+            "op": "and",
+            "content": [
+                {
+                    "op": "in",
+                    "content": {
+                        "field": "files.experimental_strategy",
+                        "value": ['RNA-Seq']
+                    }
+                }
+            ]
+        }
+
+        params = {
+            "filters": filters,
+            "fields": fields,
+            "format": "TSV",
+            "size": "200000"
+        }
+
+        response = requests.post(
+            files_endpt,
+            headers={"Content-Type": "application/json"},
+            json=params)
+
+        df = pd.read_csv(StringIO(response.content.decode("utf-8")), sep="\t")
+        df = df[
+            df['cases.0.project.project_id'].str.startswith('TCGA')]
+        df = df[
+            df['file_name'].str.endswith('rna_seq.augmented_star_gene_counts.tsv')]
+        df = df[
+            df['cases.0.samples.0.sample_type'] == 'Primary Tumor']
+
+        # When there is more than one file for a single patient just keep the first
+        # (this is assuming they are just replicates and all similar)
+        df = df[~df.duplicated(
+            subset=['cases.0.submitter_id'], keep='first')]
+
+        return df
+
+
+subjects = dict(mRNA='RNA-Seq')
+inserters = dict(mRNA=mRNADatabaseInserter)
+
+
+@app.command()
+def insert_data(subject: str = typer.Option(...), base_dir: str = typer.Option(...),
+                mongo_connection_string: str = typer.Option(...),
+                db_name: str = typer.Option(...),
+                col_name: str = typer.Option(None)
+                ):
+    col_name = col_name or subject
+    inserter = inserters[subject]
+    inserter(subject=subject,
+             base_dir=base_dir,
+             mongo_connection_string=mongo_connection_string,
+             db_name=db_name,
+             col_name=col_name)
+    
 
 if __name__ == '__main__':
     app()
